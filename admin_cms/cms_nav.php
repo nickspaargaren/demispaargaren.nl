81b73f47dbda9db138144e51a0d3dc451db4b302
Nick Spaargaren <nickspaargaren@users.noreply.github.com>
2022-10-08 16:35:19 +0200

    Update forms unread to PDO

--- admin_cms/cms_nav.php
+++ admin_cms/cms_nav.php
@@ -10,24 +10,17 @@
 
 			<?php
 			// berichten ophalen / notificatie
-			$query = "SELECT gelezen FROM formulier";
-			$sql_formulier = $mysqli->query($query) ;
-			$tab_formulier = $sql_formulier->num_rows;
+			$handle = $pdo->prepare("SELECT gelezen FROM formulier WHERE gelezen = 0");
+			$handle->execute();
+			$forms_unread = $handle->rowCount();
 
-			$resultaat_formulier = 0;
-
-			if($tab_formulier != 0) {
-				while($tab_formulier = $sql_formulier->fetch_assoc()) {
-					if ($tab_formulier['gelezen'] != 1 ){$resultaat_formulier++;}
-				}
-			}
 			?>
 			<div class="naam">
 			<?php echo $gebruikergegevens->firstname . " " . $gebruikergegevens->lastname; ?><i class="fa fa-caret-down"></i></div>
 			<ul class="submenu">
 				<li><a href="account.php">Mijn account</a></li>
 
-				<?php if($resultaat_formulier > 0){echo '<li><a href="formulier.php">Meldingen <span class="formAantal">' . $resultaat_formulier . '</span></a></li>';} ?>
+				<?php if($forms_unread > 0){echo '<li><a href="formulier.php">Meldingen <span class="formAantal">' . $forms_unread . '</span></a></li>';} ?>
 
 				<li><a href="<?php echo $base; ?>" target="_blank">Naar de homepagina</a></li>
 				<li><a href="logout.php">Uitloggen</a></li>
@@ -44,7 +37,7 @@
 		<li class="item <?php echo $cms_pagina_titel === 'Dashboard' ? 'active' : '' ?>"><a href="dashboard.php"><i class="fa fa-home"></i><span class="titel">Home</span><div class="cleared"></div></a></li>
 		<li class="item <?php echo $cms_pagina_titel === 'Pagina\'s' ? 'active' : '' ?>"><a href="paginas.php"><i class="fa fa-sitemap"></i><span class="titel">Pagina's</span><div class="cleared"></div></a></li>
 		<li class="item <?php echo $cms_pagina_titel === 'Afbeeldingen' ? 'active' : '' ?>"><a href="afbeeldingen.php"><i class="fa fa-image"></i><span class="titel">Afbeeldingen</span><div class="cleared"></div></a></li>
-		<li class="item <?php echo $cms_pagina_titel === 'Contactformulier' ? 'active' : '' ?>"><a href="formulier.php"><?php if($resultaat_formulier > 0){echo '<div class="formAantal">' . $resultaat_formulier . '</div>';} ?><i class="fa fa-file-text-o"></i><span class="titel">Contactformulier</span><div class="cleared"></div></a></li>
+		<li class="item <?php echo $cms_pagina_titel === 'Contactformulier' ? 'active' : '' ?>"><a href="formulier.php"><?php if($forms_unread > 0){echo '<div class="formAantal">' . $forms_unread . '</div>';} ?><i class="fa fa-file-text-o"></i><span class="titel">Contactformulier</span><div class="cleared"></div></a></li>
 		<li class="item <?php echo $cms_pagina_titel === 'Account' ? 'active' : '' ?>"><a href="account.php"><i class="fa fa-user"></i><span class="titel">Account</span><div class="cleared"></div></a></li>
 		<li class="item <?php echo $cms_pagina_titel === 'Social Media' ? 'active' : '' ?>"><a href="socialmedia.php"><i class="fa fa-share-alt"></i><span class="titel">Social Media</span><div class="cleared"></div></a></li>
 		<li class="item <?php echo $cms_pagina_titel === 'Instellingen' ? 'active' : '' ?>"><a href="instellingen.php"><i class="fa fa-gear"></i><span class="titel">Instellingen</span><div class="cleared"></div></a></li>
