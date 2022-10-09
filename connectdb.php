f9895d682b41f60efef77fda86f52da55cf83e8c
Nick Spaargaren <nickspaargaren@users.noreply.github.com>
2022-10-09 12:20:14 +0200

    Remove mysqli connection

--- connectdb.php
+++ connectdb.php
@@ -13,19 +13,6 @@ if (strpos($_SERVER['HTTP_HOST'], 'localhost') !== false) {
 	$base = "https://". $_SERVER['HTTP_HOST']. "/";
 }
 
-$mysqli = new mysqli(
-	$_SERVER['GLOBAL_HOSTNAME'],
-	$_SERVER['GLOBAL_USERID'],
-	$_SERVER['GLOBAL_PASSWORD'],
-	$_SERVER['GLOBAL_DATABASE']
-);
-
-$mysqli->set_charset("utf8");
-if (mysqli_connect_errno()) {
-  printf("Connect failed: %s\n", mysqli_connect_error());
-  exit();
-}
-
 $pdo = new PDO("mysql:host=" . $_SERVER['GLOBAL_HOSTNAME'] . ";dbname=" . $_SERVER['GLOBAL_DATABASE'] . ";charset=utf8mb4",
 	$_SERVER['GLOBAL_USERID'],
 	$_SERVER['GLOBAL_PASSWORD'],
