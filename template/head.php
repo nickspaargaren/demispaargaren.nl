c216fae8120921252d521c3a5d6fbbf42f5bbcbd
Nick Spaargaren <nickspaargaren@users.noreply.github.com>
2022-10-19 19:44:23 +0200

    Fix css path when sending a form

--- template/head.php
+++ template/head.php
@@ -6,7 +6,7 @@
 	<title><?php echo $currentPage->titel ?> | <?php echo $settings->projectnaam ?></title>
 	<meta name="author" content="<?php echo $settings->auteur; ?>">
 	<meta name="description" content="<?php echo $settings->description ?>">
-	<link href="template/style.css?nieuw" rel="stylesheet" type="text/css"/>
+	<link href="<?php echo $base ?>template/style.css?nieuw" rel="stylesheet" type="text/css"/>
 	<link rel="shortcut icon" type="image/x-icon" href="https://www.demispaargaren.nl/template/favicon.ico?2020"/>
 	<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
 	<script src="<?php echo $base ?>assets/components/jquery/jquery.min.js"></script>
