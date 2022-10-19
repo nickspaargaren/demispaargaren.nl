9253ed6d17279bfadbdb17f2d60fb913b3358a25
Nick Spaargaren <nickspaargaren@users.noreply.github.com>
2022-10-19 19:47:11 +0200

    Update path to favicon

--- template/head.php
+++ template/head.php
@@ -7,7 +7,7 @@
 	<meta name="author" content="<?php echo $settings->auteur; ?>">
 	<meta name="description" content="<?php echo $settings->description ?>">
 	<link href="<?php echo $base ?>template/style.css?nieuw" rel="stylesheet" type="text/css"/>
-	<link rel="shortcut icon" type="image/x-icon" href="https://www.demispaargaren.nl/template/favicon.ico?2020"/>
+	<link rel="shortcut icon" type="image/x-icon" href="<?php echo $base ?>template/favicon.ico"/>
 	<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
 	<script src="<?php echo $base ?>assets/components/jquery/jquery.min.js"></script>
 	<link rel="stylesheet" href="https://use.typekit.net/mup3tnf.css">
