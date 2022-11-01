2f02acfab2bfb9de4eaf905698155deb7646c0a5
Nick Spaargaren <nickspaargaren@users.noreply.github.com>
2022-11-01 11:42:52 +0100

    Add 404 page

---
+++ template/404.php
@@ -0,0 +1,26 @@
+<html>
+
+<head>
+    <title>404 | <? echo $settings->projectnaam ?></title>
+    <style>
+        body {
+            margin: 0;
+            height: 100%;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            text-align: center;
+            font-family: "Helvetica Neue", Helvetica, arial;
+        }
+    </style>
+    <?php echo $settings->analytics . "\n"; ?>
+</head>
+
+<body>
+    <div>
+        <h1>404</h1>
+        <p><a href="<?php echo $base; ?>">Home</a></p>
+    </div>
+</body>
+
+</html>
