1afbb5297b4d02308004a3cd810c8e2e809bdb59
Nick Spaargaren <nickspaargaren@users.noreply.github.com>
2022-11-01 11:27:10 +0100

    Add canonical url's

--- template/head.php
+++ template/head.php
@@ -1,12 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
 
+<?php
+if ($currentPage->link == 'home') {
+	$canonicalUrl = $base;
+} else {
+	$canonicalUrl = $base . $currentPage->link;
+}
+?>
+
 <head>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<!--	<?php echo $cms_versie; ?> 	-->
 	<title><?php echo $currentPage->titel ?> | <?php echo $settings->projectnaam ?></title>
 	<meta name="author" content="<?php echo $settings->auteur; ?>">
 	<meta name="description" content="<?php echo $settings->description ?>">
+	<link rel="canonical" href="<?php echo $canonicalUrl; ?>">
 	<link href="<?php echo $base ?>template/style.css?nieuw" rel="stylesheet" type="text/css" />
 	<link rel="shortcut icon" type="image/x-icon" href="<?php echo $base ?>template/favicon.ico" />
 	<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
