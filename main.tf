ee4c780e2f01efcf72ef4dfa80cccd17a4b96da7
Nick Spaargaren <nickspaargaren@users.noreply.github.com>
2024-10-08 22:58:30 +0200

    Add google_artifact_registry_repository

--- main.tf
+++ main.tf
@@ -3,28 +3,13 @@ provider "google" {
   region  = "europe-west4"
 }
 
-resource "google_project_service" "run_api" {
-  service            = "run.googleapis.com"
-  disable_on_destroy = true
-}
+resource "google_artifact_registry_repository" "demispaargaren" {
+  location      = "europe-west4"
+  repository_id = "demispaargaren"
+  description   = "Docker repository for demispaargaren.nl"
+  format        = "DOCKER"
 
-resource "google_cloud_run_v2_service" "default" {
-  name     = "demispaargaren-nl"
-  location = "europe-west4"
-
-  template {
-    containers {
-      image = "gcr.io/cloudrun/hello"
-    }
+  docker_config {
+    immutable_tags = true
   }
-
-  depends_on = [google_project_service.run_api]
-}
-
-resource "google_cloud_run_v2_service_iam_member" "noauth" {
-  location = google_cloud_run_v2_service.default.location
-  name     = google_cloud_run_v2_service.default.name
-
-  role     = "roles/run.invoker"
-  member   = "allUsers"
 }
