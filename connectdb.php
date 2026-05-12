4803b8d054a890428c09cd5a247e59444a0c8f7b
Nick Spaargaren <nickspaargaren@users.noreply.github.com>
2026-05-12 08:44:15 +0200

    Fix connectdb.php vendor autoload guard and PDO option syntax

--- connectdb.php
+++ connectdb.php
@@ -1,17 +1,17 @@
 <?php
 
-require('vendor/autoload.php');
+if (file_exists(__DIR__ . '/vendor/autoload.php')) {
+    require(__DIR__ . '/vendor/autoload.php');
 
-use Dotenv\Dotenv;
+    if (file_exists(__DIR__ . '/.env')) {
+        $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+        $dotenv->load();
+    }
 
-if (file_exists(__DIR__ . '/.env')) {
-    $dotenv = Dotenv::createImmutable(__DIR__);
-    $dotenv->load();
-}
-
-if (file_exists(__DIR__ . '/../.env')) {
-    $dotenv = Dotenv::createImmutable(__DIR__ . '/../');
-    $dotenv->load();
+    if (file_exists(__DIR__ . '/../.env')) {
+        $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+        $dotenv->load();
+    }
 }
 
 if (strpos($_SERVER['HTTP_HOST'], 'localhost') !== false) {
@@ -25,7 +25,7 @@ $pdo = new PDO(
     $_SERVER['GLOBAL_USERID'],
     $_SERVER['GLOBAL_PASSWORD'],
     [
-        PDO::ATTR_EMULATE_PREPARES, false,
+        PDO::ATTR_EMULATE_PREPARES => false,
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_PERSISTENT => false,
     ],
